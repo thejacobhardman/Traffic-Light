b2713ba759551c325553fde66a325d53539670d2
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2019-12-04 23:33:26 -0700

    Add project files.

---
+++ Traffic-Light/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_Light
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight light = new TrafficLight(TrafficLightColor.Red);
+            Console.WriteLine("Ready, Set, Go");
+            light.CurrentColor = TrafficLightColor.Green;
+
+            for (int i = 0; i < 15; i++)
+            {
+                light.change();
+                Console.WriteLine(light);
+            }
+
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Traffic-Light/TrafficLight.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Traffic_Light
+{
+    public enum TrafficLightColor
+    {
+        None,
+        Red,
+        Yellow,
+        Green
+    }
+    public class TrafficLight
+    {
+        // Auto-implemented property
+        public TrafficLightColor CurrentColor { get; set; }
+        // Random number generator
+        private Random random;
+
+
+        // Default Constructor
+        public TrafficLight()
+        {
+            //TODO: Set the Auto-implemented property CurrentColor to None
+            CurrentColor = TrafficLightColor.None;
+            random = new Random();
+        }
+
+        // Initial Value Constructor
+        public TrafficLight(TrafficLightColor lightColor)
+        {
+            //TODO: Set the Auto-implemented property CurrentColor to this function's parameter
+            CurrentColor = lightColor;
+            random = new Random();
+        }
+
+        // A method to change the light to a random value
+        public void change()
+        {
+            //TODO: Set the Auto-implemented property CurrentColor to
+            // a random color, either Red, Yellow, or Green
+            // None should not be included
+
+            Array colors = Enum.GetValues(typeof(TrafficLightColor));
+
+            CurrentColor = (TrafficLightColor)colors.GetValue(random.Next(1, colors.Length));
+        }
+
+        public override string ToString()
+        {
+            //TODO: Return the correct string based on the value of
+            // the Auto-implemented property CurrentColor
+            switch (CurrentColor)
+            {
+                case TrafficLightColor.Red:
+                    return "The Light is Red!";
+                case TrafficLightColor.Yellow:
+                    return "The Light is Yellow!";
+                case TrafficLightColor.Green:
+                    return "The Light is Green!";
+                default:
+                    return "The Light is Out of Order";
+            }
+        }
+    }
+}
